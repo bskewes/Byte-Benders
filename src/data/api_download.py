f720b4a71632948bfe75c18f185c1a598c45e744
JC <jchen20@kpmg.com.au>
2023-10-18 16:19:57 +1100

    API download edit

--- src/data/api_download.py
+++ src/data/api_download.py
@@ -11,8 +11,11 @@ print(cert_path)
 api_key = os.getenv("API_KEY")
 
 BASE = "https://api.transport.nsw.gov.au/"
-TRAIN_URL = f"{BASE}v1/gtfs/schedule/sydneytrains"
-TRAIN_REAL = f"{BASE}v2/gtfs/vehiclepos"
+# TRAIN_URL = f"{BASE}v1/gtfs/schedule/sydneytrains" # TODO - leave for testing, delete when done
+# TRAIN_REAL = f"{BASE}v2/gtfs/vehiclepos" # TODO - leave for testing, delete when done
+
+METRO_STATIC = f"{BASE}v1/gtfs/schedule/metro"
+METRO_REALTIME = f"{BASE}v1/gtfs/v1/gtfs/historical"
 
 headers = {
     "Authorization":f"apikey {api_key}"
@@ -24,6 +27,12 @@ request_details = dict(
 
 request_details['verify'] = cert_path
 
-response = requests.get(TRAIN_URL,**request_details)
-print(response)
+# response = requests.get(TRAIN_URL,**request_details)
+# print(response)
+# response = requests.get(TRAIN_REAL,**request_details)
+# print(response)
 
+response_static = requests.get(METRO_STATIC,**request_details)
+print(response_static)
+response_realtime = requests.get(METRO_REALTIME,**request_details)
+print(response_realtime)
